7b3eacfb72f8d6afb14cb17929fd1ad7e7e840ef
Fotis Paraskevopoulos <fotis@existanze.com>
2014-04-24 00:10:27 +0300

    Setting up node in dokku

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ exports.connection = function(){
 
     var url = process.env['MONGO_URL'] ? process.env['MONGO_URL'] :  'mongodb://localhost/p2pum-explicit';
 
+    console.log("Connecting to ",url);
     mongoose.connect(url);
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
